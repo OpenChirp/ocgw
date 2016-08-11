0d584a0120fd3a9f8c03e66e28a90f83f6416537
Craig Hesling <craig@hesling.com>
2016-08-11 18:54:58 -0400

    Fixed up some script.bash stuff and added the proper systemd ntp sync cmd

--- src/gwsetup/script.bash
+++ src/gwsetup/script.bash
@@ -60,14 +60,29 @@ if [ ! "$(whoami)" = "$TARGET_USER" ]; then
 fi
 
 # Get hostname of this gateway
+echo
 read -p "Please specify the gateway's number for generating the hostname(2 would mean ${HOSTNAME_PREFIX}2): " gwnumber
 hostname="${HOSTNAME_PREFIX}${gwnumber}"
 echo Hostname will be \"$hostname\"
 
 # Set new password for TARGET_USER - root show not have a password
+echo
 echo "# Please set the new password for user \"$TARGET_USER\" - root shall not have a password"
 passwd $TARGET_USER
 
+# Enable SPI using raspi-config
+echo
+echo "# Please enable SPI using the raspi-config GUI tool. We will launch it after you press enter."
+read -p "Press enter to continue " input
+$ROOT_CMD raspi-config
+
+# Change timezone tzdata
+echo
+echo "# Please set the proper time zone information using the reconfigure GUI. We will launch after you press enter"
+read -p "Press enter to continue" input
+$ROOT_CMD dpkg-reconfigure tzdata
+
+echo
 echo "# Change default ~/.vimrc to have syntax on, numbers on side, and mouse click enabled"
 cat > /home/$TARGET_USER/.vimrc <<-EOF
 	syntax on
@@ -76,6 +91,7 @@ cat > /home/$TARGET_USER/.vimrc <<-EOF
 EOF
 
 # Add .bash_aliases
+echo
 echo "# Adding standard bash aliases to ~/.bash_aliases"
 cat > /home/$TARGET_USER/.bash_aliases <<-EOF
 	# some more ls aliases
@@ -85,24 +101,37 @@ cat > /home/$TARGET_USER/.bash_aliases <<-EOF
 EOF
 
 # Upgrade to later Raspbian release
+echo
 release_original=$(lsb_release -c|awk '//{print $2}')
 echo "# Upgrading to Rasp/Debian $TARGET_DEBIAN_RELEASE"
-$ROOT_CMD sed -i "s/${release_original}/${TARGET_DEBIAN_RELEASE}/g" /etc/apt/source.list
+$ROOT_CMD sed -i "s/${release_original}/${TARGET_DEBIAN_RELEASE}/g" /etc/apt/sources.list
 $ROOT_CMD apt-get update
 $ROOT_CMD apt-get upgrade
 $ROOT_CMD apt-get dist-upgrade
 
 # Install the payload packages
+echo
 echo "# Installing the yodelgw packages form the payload"
-$ROOT_CMD dpkg -i $PAYLOAD_DIR/*.deb
+$ROOT_CMD dpkg -i $PAYLOAD_DIR/pkgs/builds/*.deb
 $ROOT_CMD apt-get install -f
 
+# Force NTP to sync
+echo
+echo "# Adjusting the time from the NTP server"
+#$ROOT_CMD service ntp stop
+#$ROOT_CMD ntpd -gq
+#$ROOT_CMD service ntp start
+$ROOT_CMD timedatectl set-ntp yes
+
 # Change hostname
+echo
 hostname_original=`hostname`
 echo "# Changing hostname $hostname_original to $hostname in hosts and hostname files"
 $ROOT_CMD sed -i "s/${hostname_original}/${hostname}/g" /etc/hostname
 $ROOT_CMD sed -i "s/${hostname_original}/${hostname}/g" /etc/hosts
 
+
+echo
 echo "## Showing final settings ##"
 echo
 echo "Hostname: $hostname"
