ce23f167145359e25295a2438c7a958a2d775b5f
Craig Hesling <craig@hesling.com>
2017-11-07 17:55:45 -0500

    Added optional parameters for compiling pktfwd

--- src/sourceme.bash
+++ src/sourceme.bash
@@ -1,3 +1,5 @@
+#export CROSS_COMPILE="arm-linux-gnueabi-"
+#export CFLAGS="-mfloat-abi=soft -O2 -Wall -Wextra -std=c99 -Iinc -I." 
 _TeMp=$(readlink -f $BASH_SOURCE)
 export GOPATH="$(dirname $_TeMp)/go"
 export GOARCH=arm
